7f14d5a658278cb4554bde5b2e959f2630ce6b59
highher <a01068503428@gmail.com>
2022-10-18 15:43:26 +0900

    first

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {Typography, Button, Form, Input} from 'antd';
+import FileUpload from '../../utils/FileUpload';
 
 const {Title} = Typography;
 const {TextArea} = Input;
